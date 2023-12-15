fb2f20c48772e492fcf0b1d9fe16eebe9094062d
Roman Kozak <roman.gruit@gmail.com>
2023-12-15 10:18:45 +0200

    Updated docs

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # HS-GPT-bot
 
-This repo is an addition to the article on the HubSpot dev blog. Read article for better understanding here.
+This repo is an addition to the article on the HubSpot dev blog. Read article for better understanding on how to use the code.
 
-## Instalation
+## Installation
 
 Run ```npm i``` command in your terminal to install dependencies.
 
 Then run ```npm run build``` to get the file for copying to a HubSpot chat function.
 
-Open ```dist``` folder and copy everything from the ```index.js``` file inside the ```dist``` folder.
+Open ```dist``` folder and copy everything from the ```index.js``` file.
 
 Paste the code into the HubSpot chat function.
+
+## Code Modification
+
+Open ```index.js```.
+
+Change the code as you see it fits.
+
+Run ```npm run build``` to get the modified file for copying to a HubSpot chat function.
--- index.js
+++ index.js
@@ -1,44 +1,55 @@
+// This line is loading the OpenAI official sdk library
 const OpenAI = require("openai");
 
+// This line is setting the OPENAI_API_KEY and ASSISTANT_ID constants from the environment variables
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const ASSISTANT_ID = process.env.ASSISTANT_ID;
 
+// This line is creating a new instance of the OpenAI client. It allows us to call the OpenAI APIs.
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY
 });
 
-async function main(event = {}, callback = () => {}) {
-  // console.log({ event });
-
+// This is the main function that is called by HubSpot when a message is received from the user.
+async function main(event = {}, callback) {
+  // This line is extracting the user message from the event object
   const userMessageStr = event.userMessage?.message || "";
+
+  // This line is extracting the threadId from the event object. This is used to keep track of the conversation.
   const session = event.session;
   let threadId = session?.customState?.threadId;
 
-  // create a new thread if it is a new session
+  // Create a new thread by using OpenAI sdk client if it is a new session. Otherwise, use the existing thread id.
   if (!threadId) {
     const thread = await openai.beta.threads.create();
     threadId = thread.id;
   }
 
+  // This line is sending the user message to the OpenAI API to save it in the thread. Assistant answer generation is not happening here.
   await openai.beta.threads.messages.create(threadId, {
     role: "user",
     content: userMessageStr
   });
 
+  // This line is trigger OpenAI API to start generate an answer from the assistant. It might take a few seconds to complete.
   let run = await openai.beta.threads.runs.create(threadId, {
     assistant_id: ASSISTANT_ID
   });
 
+  // This loop is checking the status of the answer generation by sending a request to the OpenAI API. It will keep checking until the status is completed.
   do {
     run = await openai.beta.threads.runs.retrieve(threadId, run.id);
   } while (
     !["cancelled", "failed", "completed", "expired"].includes(run.status)
   );
 
+  // This line is getting the answer from the OpenAI API.
   const messages = await openai.beta.threads.messages.list(threadId);
 
+  // This line is extracting the answer from the response.
   const content = messages.data[0].content;
 
+  // This line is preparing the response to HubSpot. It contains the answer from the assistant, and the threadId to keep track of the conversation.
   const responseJson = {
     botMessage: content[0].text.value,
     responseExpected: true,
@@ -47,9 +58,9 @@ async function main(event = {}, callback = () => {}) {
     }
   };
 
+  // This line is sending the response back to HubSpot chat bot.
   callback(responseJson);
 }
 
-// main();
-
+// This line is exporting the main function so that HubSpot can call it.
 exports.main = main;
